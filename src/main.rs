d00964ba58e36071c7e36bbd2a99a278953a1c30
Jon Deaves <hello@jondeaves.me>
2021-06-02 00:09:36 +0100

    chore: next step

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use std::env;
+use std::{env, fs};
 
+/*
+Excert about what main.rs should do, from https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html
+
+The responsibilities that remain in the main function after this process should be limited to the following:
+    Calling the command line parsing logic with the argument values
+    Setting up any other configuration
+    Calling a run function in lib.rs
+    Handling the error if run returns an error
+*/
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for '{}'", query);
+    println!("In file '{}'", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
